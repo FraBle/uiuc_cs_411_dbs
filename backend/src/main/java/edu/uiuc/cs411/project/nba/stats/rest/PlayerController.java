63d68f37b8c4abd290b77eb4a1e4a8a4a35ac2ba
Frank Blechschmidt <contact@frank-blechschmidt.com>
2020-03-07 20:22:33 -0800

    Enable CORS for local development

--- backend/src/main/java/edu/uiuc/cs411/project/nba/stats/rest/PlayerController.java
+++ backend/src/main/java/edu/uiuc/cs411/project/nba/stats/rest/PlayerController.java
@@ -3,6 +3,7 @@ package edu.uiuc.cs411.project.nba.stats.rest;
 import edu.uiuc.cs411.project.nba.stats.domain.Player;
 import edu.uiuc.cs411.project.nba.stats.query.PlayerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 public class PlayerController {
 
