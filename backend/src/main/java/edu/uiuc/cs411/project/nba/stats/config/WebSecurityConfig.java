e6fbc581f5fd6a365443a6588111989b377c1d4c
Frank Blechschmidt <contact@frank-blechschmidt.com>
2020-04-04 23:55:40 -0700

    Fix Web Security config order

--- backend/src/main/java/edu/uiuc/cs411/project/nba/stats/config/WebSecurityConfig.java
+++ backend/src/main/java/edu/uiuc/cs411/project/nba/stats/config/WebSecurityConfig.java
@@ -71,9 +71,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(authEntryPointJwt).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/test/**").permitAll()
+			.authorizeRequests()
 			.antMatchers(staticResources).permitAll()
+			.antMatchers("/api/auth/**").permitAll()
+			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
