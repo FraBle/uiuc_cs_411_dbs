2953b204c2c6cf2ea5f932b755473e9be4b6b4bb
Zhe He <zhe@coursera.org>
2020-04-20 21:04:09 -0700

    add PlayerGameStats and views for player stats query

--- data-preprocessing/PlayerGameStats.sql
+++ data-preprocessing/PlayerGameStats.sql
@@ -1,12 +1,28 @@
-CREATE TABLE `Games` (
-  `ID` int(11) NOT NULL,
-  `Season` year(4) NOT NULL,
-  `HomeFranchise` int(11)  NOT NULL,
-  `VisitorFranchise` int(11)  NOT NULL,
-  `Date` date NOT NULL,
-  PRIMARY KEY (`ID`),
-  FOREIGN KEY (`HomeFranchise`) REFERENCES `Franchise` (`ID`),
-  FOREIGN KEY (`VisitorFranchise`) REFERENCES `Franchise` (`ID`)
+  CREATE TABLE `PlayerGameStats` (
+  `Game` int(11) NOT NULL,
+  `Player` int(11) NOT NULL,
+  `Franchise` int(11) NOT NULL,
+  `MinutesPlayed` tinyint(4) NOT NULL,
+  `FieldGoalsMade` tinyint(4) NOT NULL,
+  `FieldGoalsAttempted` tinyint(4) NOT NULL,
+  `ThreePointersMade` tinyint(4) NOT NULL,
+  `ThreePointersAttempted` tinyint(4) NOT NULL,
+  `FreeThrowsMade` tinyint(4) NOT NULL,
+  `FreeThrowsAttempted` tinyint(4) NOT NULL,
+  `OffensiveRebounds` tinyint(4) NOT NULL,
+  `DefensiveRebounds` tinyint(4) NOT NULL,
+  `Points` tinyint(4) NOT NULL,
+  `Assists` tinyint(4) NOT NULL,
+  `Steals` tinyint(4) NOT NULL,
+  `Blocks` tinyint(4) NOT NULL,
+  `Turnovers` tinyint(4) NOT NULL,
+  `PersonalFouls` tinyint(4) NOT NULL,
+  KEY `Game` (`Game`),
+  KEY `Player` (`Player`),
+  KEY `Franchise` (`Franchise`),
+  CONSTRAINT `PlayerGameStats_ibfk_1` FOREIGN KEY (`Game`) REFERENCES `Games` (`ID`),
+  CONSTRAINT `PlayerGameStats_ibfk_2` FOREIGN KEY (`Player`) REFERENCES `Player` (`ID`),
+  CONSTRAINT `PlayerGameStats_ibfk_3` FOREIGN KEY (`Franchise`) REFERENCES `Franchise` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOAD DATA LOCAL INFILE './PlayerGameStats.csv' 
