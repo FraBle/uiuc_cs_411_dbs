c13825cff77ad6c610866f7cc48d2e9d3fd52607
Frank Blechschmidt <contact@frank-blechschmidt.com>
2020-04-26 23:38:34 -0700

    Add franchise details to games from calendar API

--- backend/src/main/java/edu/uiuc/cs411/project/nba/stats/domain/Game.java
+++ backend/src/main/java/edu/uiuc/cs411/project/nba/stats/domain/Game.java
@@ -7,46 +7,100 @@ public class Game {
   private Integer season;
   private Integer homeFranchise;
   private Integer visitorFranchise;
-  private Date data;
+  private Date date;
+  private String HomeAbbreviation;
+  private String HomeCity;
+  private String HomeNickname;
+  private String VisitorAbbreviation;
+  private String VisitorCity;
+  private String VisitorNickname;
 
   public Integer getId() {
-    return id;
+    return this.id;
   }
 
-  public void setId(final Integer id) {
+  public void setId(Integer id) {
     this.id = id;
   }
 
   public Integer getSeason() {
-    return season;
+    return this.season;
   }
 
-  public void setSeason(final Integer season) {
+  public void setSeason(Integer season) {
     this.season = season;
   }
 
   public Integer getHomeFranchise() {
-    return homeFranchise;
+    return this.homeFranchise;
   }
 
-  public void setHomeFranchise(final Integer homeFranchise) {
+  public void setHomeFranchise(Integer homeFranchise) {
     this.homeFranchise = homeFranchise;
   }
 
   public Integer getVisitorFranchise() {
-    return visitorFranchise;
+    return this.visitorFranchise;
   }
 
-  public void setVisitorFranchise(final Integer visitorFranchise) {
+  public void setVisitorFranchise(Integer visitorFranchise) {
     this.visitorFranchise = visitorFranchise;
   }
 
-  public Date getData() {
-    return data;
+  public Date getDate() {
+    return this.date;
   }
 
-  public void setData(final Date data) {
-    this.data = data;
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public String getHomeAbbreviation() {
+    return this.HomeAbbreviation;
+  }
+
+  public void setHomeAbbreviation(String HomeAbbreviation) {
+    this.HomeAbbreviation = HomeAbbreviation;
+  }
+
+  public String getHomeCity() {
+    return this.HomeCity;
+  }
+
+  public void setHomeCity(String HomeCity) {
+    this.HomeCity = HomeCity;
+  }
+
+  public String getHomeNickname() {
+    return this.HomeNickname;
+  }
+
+  public void setHomeNickname(String HomeNickname) {
+    this.HomeNickname = HomeNickname;
+  }
+
+  public String getVisitorAbbreviation() {
+    return this.VisitorAbbreviation;
+  }
+
+  public void setVisitorAbbreviation(String VisitorAbbreviation) {
+    this.VisitorAbbreviation = VisitorAbbreviation;
+  }
+
+  public String getVisitorCity() {
+    return this.VisitorCity;
+  }
+
+  public void setVisitorCity(String VisitorCity) {
+    this.VisitorCity = VisitorCity;
+  }
+
+  public String getVisitorNickname() {
+    return this.VisitorNickname;
+  }
+
+  public void setVisitorNickname(String VisitorNickname) {
+    this.VisitorNickname = VisitorNickname;
   }
 
 }
--- backend/src/main/java/edu/uiuc/cs411/project/nba/stats/query/GameMapper.java
+++ backend/src/main/java/edu/uiuc/cs411/project/nba/stats/query/GameMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface GameMapper {
 
-  @Select("SELECT * FROM Games WHERE MONTH(`date`)=${month} AND YEAR(`date`)=${year} ORDER BY `date`")
+  @Select("SELECT Games.*, HomeFranchise.Abbreviation as HomeAbbreviation, HomeFranchise.City as HomeCity, HomeFranchise.Nickname as HomeNickname, VisitorFranchise.Abbreviation as VisitorAbbreviation, VisitorFranchise.City as VisitorCity, VisitorFranchise.Nickname as VisitorNickname FROM Games JOIN Franchise HomeFranchise ON Games.HomeFranchise= HomeFranchise.ID JOIN Franchise VisitorFranchise ON Games.VisitorFranchise= VisitorFranchise.ID WHERE MONTH(`date`)=${month} AND YEAR(`date`)=${year} ORDER BY `date`, Games.id")
   List<Game> getGamesByMonthYear(@Param("month") Integer month, @Param("year") Integer year);
 
 }
