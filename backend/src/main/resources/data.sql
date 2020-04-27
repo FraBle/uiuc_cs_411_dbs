641b99f22d27fb03377b63c17760d2bb41b2ef25
Frank Blechschmidt <contact@frank-blechschmidt.com>
2020-04-26 23:50:34 -0700

    Fix data for tests (foreign key)

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,45 +1,60 @@
 -- Temporary create some Dummy data to test the mapper
 INSERT INTO Player
-VALUES (1, 'Michael Jordan', parseDateTime('19630217000000','yyyyMMddHHmmss'), 'F-G', '6.6', 216, NULL);
+VALUES
+    (1, 'Michael Jordan', parseDateTime('19630217000000','yyyyMMddHHmmss'), 'F-G', '6.6', 216, NULL);
 
 INSERT INTO Player
-VALUES (2, 'Kobe Bryant', parseDateTime('19780823000000','yyyyMMddHHmmss'), 'F-G', '6.6', 212, NULL);
+VALUES
+    (2, 'Kobe Bryant', parseDateTime('19780823000000','yyyyMMddHHmmss'), 'F-G', '6.6', 212, NULL);
 
 INSERT INTO Player
-VALUES (3, 'LeBron James', parseDateTime('19841230000000','yyyyMMddHHmmss'), 'G-F', '6.9', 250, NULL);
+VALUES
+    (3, 'LeBron James', parseDateTime('19841230000000','yyyyMMddHHmmss'), 'G-F', '6.9', 250, NULL);
 
 INSERT INTO Player
-VALUES (4, 'Stephen Curry', parseDateTime('198803140000000','yyyyMMddHHmmss'), 'G', '6.3', 190, NULL);
+VALUES
+    (4, 'Stephen Curry', parseDateTime('198803140000000','yyyyMMddHHmmss'), 'G', '6.3', 190, NULL);
 
 INSERT INTO Player
-VALUES (5, 'Kevin Durant', parseDateTime('19630217000000','yyyyMMddHHmmss'), 'F', '6.10', 240, NULL);
+VALUES
+    (5, 'Kevin Durant', parseDateTime('19630217000000','yyyyMMddHHmmss'), 'F', '6.10', 240, NULL);
 
 INSERT INTO Games
-VALUES (100, 2018, 1001, 1003, parseDateTime('20180218000000','yyyyMMddHHmmss'));
+VALUES
+    (100, 2018, 1, 2, parseDateTime('20180218000000','yyyyMMddHHmmss'));
 
 INSERT INTO Games
-VALUES (101, 2019, 1001, 1002, parseDateTime('20190217000000','yyyyMMddHHmmss'));
+VALUES
+    (101, 2019, 2, 1, parseDateTime('20190217000000','yyyyMMddHHmmss'));
 
 INSERT INTO Games
-VALUES (102, 2019, 1001, 1003, parseDateTime('20190218000000','yyyyMMddHHmmss'));
+VALUES
+    (102, 2019, 1, 2, parseDateTime('20190218000000','yyyyMMddHHmmss'));
 
 INSERT INTO PlayerGameStats
-VALUES (100, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+VALUES
+    (100, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
 INSERT INTO PlayerGameStats
-VALUES (101, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+VALUES
+    (101, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
 INSERT INTO PlayerGameStats
-VALUES (102, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+VALUES
+    (102, 3, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
 INSERT INTO PlayerGameStats
-VALUES (100, 2, 1002, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100, 10, 10, 10, 10, 10);
+VALUES
+    (100, 2, 1002, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100, 10, 10, 10, 10, 10);
 
 INSERT INTO PlayerGameStats
-VALUES (100, 1, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10);
+VALUES
+    (100, 1, 1001, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10);
 
 INSERT INTO Franchise
-VALUES (1, 'GSW', 'Warriors', 1946, 'Golden State', 'Chase Center');
+VALUES
+    (1, 'GSW', 'Warriors', 1946, 'Golden State', 'Chase Center');
 
 INSERT INTO Franchise
-VALUES (2, 'LAL', 'Lakers', 1948, 'Los Angeles', 'Staples Center');
+VALUES
+    (2, 'LAL', 'Lakers', 1948, 'Los Angeles', 'Staples Center');
