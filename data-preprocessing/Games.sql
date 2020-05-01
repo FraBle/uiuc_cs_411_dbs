2317f22b08ef3b4756b0c70f7863d0910fc463ee
Zhe He <zhe@coursera.org>
2020-04-30 19:53:47 -0700

    load more game details into mysql

--- data-preprocessing/Games.sql
+++ data-preprocessing/Games.sql
@@ -1,12 +1,26 @@
 CREATE TABLE `Games` (
   `ID` int(11) NOT NULL,
   `Season` year(4) NOT NULL,
-  `HomeFranchise` int(11)  NOT NULL,
-  `VisitorFranchise` int(11)  NOT NULL,
+  `HomeFranchise` int(11) NOT NULL,
+  `VisitorFranchise` int(11) NOT NULL,
   `Date` date NOT NULL,
+  `HomePoints` smallint(6) NOT NULL,
+  `HomeFieldGoalPercentage` float NOT NULL,
+  `HomeFreeThrowPercentage` float NOT NULL,
+  `HomeThreePointerPercentage` float NOT NULL,
+  `HomeAssists` smallint(6) NOT NULL,
+  `HomeRebounds` smallint(6) NOT NULL,
+  `AwayPoints` smallint(6) NOT NULL,
+  `AwayFieldGoalPercentage` float NOT NULL,
+  `AwayFreeThrowPercentage` float NOT NULL,
+  `AwayThreePointerPercentage` float NOT NULL,
+  `AwayAssists` smallint(6) NOT NULL,
+  `AwayRebounds` smallint(6) NOT NULL,
   PRIMARY KEY (`ID`),
-  FOREIGN KEY (`HomeFranchise`) REFERENCES `Franchise` (`ID`),
-  FOREIGN KEY (`VisitorFranchise`) REFERENCES `Franchise` (`ID`)
+  KEY `HomeFranchise` (`HomeFranchise`),
+  KEY `VisitorFranchise` (`VisitorFranchise`),
+  CONSTRAINT `Games_ibfk_1` FOREIGN KEY (`HomeFranchise`) REFERENCES `Franchise` (`ID`),
+  CONSTRAINT `Games_ibfk_2` FOREIGN KEY (`VisitorFranchise`) REFERENCES `Franchise` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOAD DATA LOCAL INFILE './Games.csv' 
